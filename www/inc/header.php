6691306e4b588381255edd475a0a71c6c7c8aa62
robertmcbathin <mercile55@yandex.ru>
2016-04-05 10:32:52 +0300

    header is ready

--- www/inc/header.php
+++ www/inc/header.php
@@ -15,6 +15,7 @@
     <link href="frontside/css/prettyPhoto.css" rel="stylesheet">
     <link href="frontside/css/main.css" rel="stylesheet">
     <link href="frontside/css/responsive.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
--- www/index.php
+++ www/index.php
@@ -9,23 +9,14 @@ include_once 'inc/header.php';
             <div class="container">
                 <div class="row">
                     <div class="col-sm-6 col-xs-4">
-                        <div class="top-number"><p><i class="fa fa-phone-square"></i>  +0123 456 70 90</p></div>
+                        <div class="top-number">
+                        <p class="text-grey"><i class="fa fa-phone-square"></i>  8 (8352) 21-77-66</p>
+                        <p class="text-grey"><i class="fa fa-phone-square"></i>  8 (8352) 21-66-99</p>
+                        </div>
                     </div>
                     <div class="col-sm-6 col-xs-8">
                        <div class="social">
-                            <ul class="social-share">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li> 
-                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                                <li><a href="#"><i class="fa fa-skype"></i></a></li>
-                            </ul>
-                            <div class="search">
-                                <form role="form">
-                                    <input type="text" class="search-form" autocomplete="off" placeholder="Search">
-                                    <i class="fa fa-search"></i>
-                                </form>
-                           </div>
+                            <a href="registry" class="btn btn-default registry-button">ЗАПИСАТЬСЯ НА ПРИЕМ СЕЙЧАС!</a>
                        </div>
                     </div>
                 </div>
@@ -46,21 +37,19 @@ include_once 'inc/header.php';
 				
                 <div class="collapse navbar-collapse navbar-right">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="index.html">Home</a></li>
-                        <li><a href="about-us.html">About Us</a></li>
-                        <li><a href="services.html">Services</a></li>
-                        <li><a href="portfolio.html">Portfolio</a></li>
+                        <li class="active"><a href="index">Главная</a></li>
+                        <li><a href="about-us">О поликлинике</a></li>
+                        <li><a href="services">Услуги и цены</a></li>
+                        <li><a href="documents">Документы</a></li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="fa fa-angle-down"></i></a>
+                            <a href="preparation" class="dropdown-toggle" data-toggle="dropdown">Подготовка <i class="fa fa-angle-down"></i></a>
                             <ul class="dropdown-menu">
-                                <li><a href="blog-item.html">Blog Single</a></li>
-                                <li><a href="pricing.html">Pricing</a></li>
-                                <li><a href="404.html">404</a></li>
-                                <li><a href="shortcodes.html">Shortcodes</a></li>
+                                <li><a href="fgds-preparation">ФГДС</a></li>
+                                <li><a href="fks-preparation">Колоноскопия</a></li>
+                                <li><a href="prokto-preparation">Проктология</a></li>
                             </ul>
                         </li>
-                        <li><a href="blog.html">Blog</a></li> 
-                        <li><a href="contact-us.html">Contact</a></li>                        
+                        <li><a href="contacts">Контакты</a></li>                        
                     </ul>
                 </div>
             </div><!--/.container-->
